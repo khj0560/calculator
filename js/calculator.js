8795a110ad0c0cb0acedfa69f8f09f111b05fa57
khj0560 <khj0560@naver.com>
2018-11-11 23:21:57 +0900

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -52,12 +52,12 @@ window.onload = function(){
                 }else if(theNum) {
                     theNum += dataNum;
                 };
-                theNum = (theNum.charAt(0)=='0' && theNum.charAt(1)!='.') ? theNum.substring(1) : theNum;
+                theNum = (theNum.length > 1 && theNum.charAt(0)=='0' && theNum.charAt(1)!='.') ? theNum.substring(1) : theNum;
         }    
-        
+        console.log('setNum내부 dataNum: '+dataNum);
+        console.log('setNum내부 theNum: '+theNum);
         //viewer에 입력값 세팅
         if(theNum) {
-            //viewer.innerHTML = (theNum.indexOf('.')>0) ? theNum : theNum.replace(addexp, ',');
             view(theNum);
         }    
     };
@@ -122,7 +122,7 @@ window.onload = function(){
                 resultNum = resultNum.toFixed(8).toString();
             }
 
-            viewer.innerHTML = (resultNum.indexOf('.')>0) ? resultNum : resultNum.replace(addexp, ',');
+            view(resultNum);
             result.setAttribute('data-result', resultNum);
             oldNum = resultNum;
             theNum = '';
@@ -133,12 +133,12 @@ window.onload = function(){
         }
     };
     
-    var view = function(str) {
-        
+    var view = function(str) {        
         viewer.innerHTML = (str.indexOf('.')>0) ? str : str.replace(addexp, ',');
     }
     
     var viewError = function() {
+        console.log('inError');
         viewer.innerHTML = 'Error';
         theNum = '';
         oldNum = '';
@@ -167,37 +167,3 @@ window.onload = function(){
 };
 
 
-
-//.querySelector()는 CSS 선택자로 요소를 선택하게 해줍니다. 주의할 점은 선택자에 해당하는 첫번째 요소만 선택한다는 것입니다.
-/*	
-document.querySelector( 'selector' )
-document.querySelector( '.xyz' )
-는 클래스 값이 xyz인 첫번째 요소에 접근합니다.
-*/
-
-//.querySelectorAll()은 특정 CSS 선택자를 가진 모든 요소를 배열로 가져오는 메서드입니다.
-/*
-document.querySelectorAll( 'selector' )
-
-예를 들어
-document.querySelectorAll( '.abc' )
-는 클래스 값이 abc인 모든 요소를 가져옵니다.
-document.querySelectorAll( '.abc, .def' );
-클래스 값이 abc 또는 def인 모든 요소를 가져옵니다.
-
-
-
-
-웹문서를 만드는 경우, 문서가 로드되었을때를 기점으로 문서를 초기화하고, 각종 설정을 부여하는 것은 빈번한 일입니다. 이 시점에 접근하기 위해 사용되는 이벤트들에 대하여 알아봅니다.
-
-onload
-문서의 모든 콘텐츠(images, script, css, etc)가 로드된 후 발생하는 이벤트이다(load 이벤트라고들 한다).
-window.onload = function() { //실행될 코드 }
-
-window.onload같은 경우 전체 페이지의 모든 외부리소스와 이미지가 브라우저에 불리워진 이후에 작동을 하게 되어 이미지가 안뜨거나 딜레이가 생길때는 그 만큼의 시간을 기다려야 하는 문제가 있습니다.
-    
-출처: http://webdir.tistory.com/515 [WEBDIR]
-
-JavaScript 부모요소와 자식요소에 같은 이벤트를 바인딩했을 때
-Event Bubbling
-*/
